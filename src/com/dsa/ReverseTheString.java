28c52cb7135771fb7caef2f650f9011912f94ea0
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-08-07 20:38:21 +0530

    ADD - Day6: Reverse the string using recursive method.

---
+++ src/com/dsa/ReverseTheString.java
@@ -0,0 +1,27 @@
+package com.dsa;
+
+public class ReverseTheString {
+    public static void main(String[] args) {
+
+        String testcase1 = "one two three";
+        String testcase2 = "I love India";
+
+        System.out.println(reverseTheString(testcase1));
+        System.out.println(reverseTheString(testcase2));
+    }
+
+    private static String reverseTheString(String inputString) {
+
+        inputString = inputString.trim();
+        int indexOfSpace = inputString.indexOf(" ");
+
+        if(indexOfSpace == -1) {
+            return inputString;
+        }
+
+        String firstWord = inputString.substring(0, indexOfSpace);
+        String restOfTheWord = inputString.substring(indexOfSpace);
+
+        return reverseTheString(restOfTheWord)  + " " + firstWord;
+    }
+}
